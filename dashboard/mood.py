a14abc4969b436ae5653c7802536a850700e0e1c
TheCookieNoid <dhanushkarguin10@gmail.com>
2025-08-11 16:48:00 +0530

    Correctly predicts the mood now and new pipeline and model is introduced

--- dashboard/mood.py
+++ dashboard/mood.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from dotenv import load_dotenv
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from transformers import pipeline
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 CHATBOT_FOLDER = os.path.join(PROJECT_ROOT, 'chatbot')
@@ -15,7 +15,7 @@ user_input = app.user_input
 load_dotenv('.env.dashboard')
 access_token = os.getenv('API_KEY')
 
-model = AutoModelForSequenceClassification.from_pretrained("boltuix/bert-emotion", token=access_token)
-tokenizer = AutoTokenizer.from_pretrained("boltuix/bert-emotion", token=access_token)
+sentiment_analysis = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", token=access_token)
 
-print(user_input)
+result = sentiment_analysis(user_input)[0]
+print(result)
